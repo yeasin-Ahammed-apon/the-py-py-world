7ac9b0574a3df4c043cd11d1353f6fc82ee6d030
yeasin-Ahammed-apon <yeasinahammedapon@gmail.com>
2024-09-15 16:28:23 +0600

    d1p1

---
+++ sept_15.py
@@ -0,0 +1,57 @@
+# 3.1. Using Python as a Calculator
+
+
+# # 3.1.1. Numbers
+# print("plus =",2+2)
+# print("Minus =",2-2)
+# print("divide =",2/2)
+# print("modulas =",2%2)
+# print("power=",2**2)
+# print("floors the divided result =",2// 2) 
+
+    # # it only work in terminal here the _ is for catching the last calculated value
+# tax = 12.5 / 100
+# price = 100.50
+# price * tax
+# print(price + _)
+
+
+
+# 3.1.2. Text
+print("\"most of the text thing \n is same as js , php and c++\"..")
+print("lets have a look in other things , that \t\t\t\t are not same")
+print("""
+      this is for multiline thing
+      this is good for multiline thing .
+      like it.
+      """)
+    # repeating string 
+print(3 *  "this is fun \n")
+    # string next to each other automaticly concatinated
+print('yeasin' ' ahammed ' + "apon")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
